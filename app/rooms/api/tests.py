f3fb4264712d57ee4acbd18a7b75206f9b7db467
mddarr <mddarr@gmail.com>
2020-03-22 11:14:13 -0700

    narrowing in on the testing client problem..

--- app/rooms/api/tests.py
+++ app/rooms/api/tests.py
@@ -9,12 +9,14 @@ class RoomTestCase(APITestCase):
 
     def test_create_room(self):
         response = self.client.post(
-            path='http://127.0.0.1:8000/rooms/create_room/',
+            path="http://127.0.0.1:8000/rooms/create_room/",
             data=json.dumps({
-                'host':1
+                "host":1
             }),
             content_type='application/json'
         )
-        self.assertEqual(response.status_code, 201)
+
         #, 'state':'Arkansas','city':'bonerville'}
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        print(response.data)
+        #self.assertEqual(response.status_code, status.HTTP_201_CREATED)
