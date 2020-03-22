3924f9607ab264459c24bd754463368eb0e17262
mddarr <mddarr@gmail.com>
2020-03-22 15:45:01 -0700

    Seed the rooms & hosts

--- app/utils/test_utils/date_seeder.py
+++ app/utils/test_utils/date_seeder.py
@@ -16,37 +16,12 @@ class DataSeeder:
                     line_count += 1
                 else:
                     try:
-                        host = Host()
-
-                        username = row[0]
-                        host.username=username
-                        password = row[1]
-                        host.password=password
-                        first_name = row[2]
-                        host.first_name = first_name
-                        last_name = row[3]
-                        host.last_name = last_name
-                        email = row[5]
-                        host.email = email
-                        phone_number = row[6]
-                        host.phone_number=phone_number
-                        state = row[7]
-                        host.state=state
-                        city = row[8]
-                        host.city = city
-
-                        address = row[9]
-                        host.address=address
-
-                        zip = int(row[10])
-                        host.zip = zip
-
-                        host.save()
-
+                        DataSeeder.parse_host_csv_row(row)
                         line_count += 1
                     except Exception as e:
                         print(e)
 
+    @staticmethod
     def seed_rooms(rooms_csv):
         with open(rooms_csv) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
@@ -57,60 +32,51 @@ class DataSeeder:
                     line_count += 1
                 else:
                     try:
-                        room = Room()
-                        hostID = int(row[0])+29
-                        host = Host.objects.get(id=hostID)
-                        room.host = host
-
-                        name = row[1]
-                        room.name = name
-
-
-                        address = row[2]
-                        room.name = address
-
-                        city = row[3]
-                        room.city = city
-
-                        state = row[4]
-                        room.name = state
-
-                        lat = row[5]
-                        room.name = float(lat)
-
-                        lng = row[6]
-                        room.long = float(lng)
-                        room.save()
-                        property_type = row[7]
-                        room.property_type = property_type
-
-                        guest_count = row[8]
-                        room.guest_count = guest_count
-
-                        bed_count = row[9]
-                        room.bed_count = bed_count
-
-                        bath_count = row[10]
-                        room.bath_count = bath_count
-
-                        description = row[11]
-                        room.description = description
-
-                        available_date = row[12]
-                        room.available_date = available_date
-
-                        price = row[13]
-                        room.price = int(price)
-
-                        published = row[14]
-                        room.published = published
-
-                        room.save()
+                        DataSeeder.parse_room_csv_row(row)
                         print("save a room")
                         line_count += 1
                     except Exception as e:
                         print(e)
 
+    @staticmethod
+    def parse_host_csv_row(row):
+        host = Host()
 
-    seed_rooms('rooms.csv')
+        host.username = row[0]
+        host.password = row[1]
+        host.first_name = row[2]
+        host.last_name = row[3]
+        host.email = row[4]
+        host.phone_number = row[5]
+        host.state = row[6]
+        host.city = row[7]
+        print("row 8 " + row[8])
+        # host.address = row[8]
+        host.zip = int(row[9])
+        host.save()
+
+    @staticmethod
+    def parse_room_csv_row(row):
+        room = Room()
+        hostID = int(row[0]) + 44
+        host = Host.objects.get(id=hostID)
+        room.host = host
+
+        room.name = row[1]
+        room.address = row[2]
+        room.city = row[3]
+        room.state = row[4]
+        room.lat = float(row[5])
+        room.long = float(row[6])
+        room.property_type = row[7]
+        room.guest_count = row[8]
+        room.bed_count = row[9]
+        room.bath_count = row[10]
+        room.description = row[11]
+        room.available_date = row[12]
+        room.price = int(row[13])
+        room.published = row[14]
+        room.save()
+#DataSeeder.seed_host('hosts.csv')
+DataSeeder.seed_rooms('rooms.csv')
 
