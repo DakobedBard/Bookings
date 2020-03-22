ddc4520f7a74a02454faf4eb963bead7edb1c500
mddarr <mddarr@gmail.com>
2020-03-22 13:08:51 -0700

    Add a data seeder for the hosts

--- app/users/models.py
+++ app/users/models.py
@@ -19,39 +19,9 @@ class Host(models.Model):
     first_name = models.CharField(max_length=30, default="Joseph")
     last_name = models.CharField(max_length=30, default="Dulapp")
     email = models.CharField(max_length=30, default="Nebrasa")
-    phone_number = models.CharField(max_length=15, default=" ")
+    phone_number = models.CharField(max_length=35, default=" ")
     address = models.CharField(max_length=60, default='8009 39th AVE NE')
     city = models.CharField(max_length=30, default='Seattle')
     state = models.CharField(max_length=30, default='Washington')
     country = models.CharField(max_length=30, default='United States')
     zip = models.IntegerField( default=98115)
-
-
-
-guest_table_create = (""" CREATE TABLE IF NOT EXISTS guests(
-                            guest_id serial PRIMARY KEY ,
-                            name CHAR NOT NULL,
-                            email CHAR NOT NULL,
-                            phone CHAR NOT NULL,
-                            username CHAR NOT NULL,
-                            password CHAR NOT NULL,
-                            address CHAR NOT NULL,
-                            city CHAR NOT NULL,
-                            state CHAR NOT NULL,
-                            country CHAR NOT NULL,
-                            zip INT NOT NULL
-                        );""")
-
-host_table_create = (""" CREATE TABLE IF NOT EXISTS hosts(
-                            host_id serial PRIMARY KEY ,
-                            name CHAR NOT NULL,
-                            email CHAR NOT NULL,
-                            phone CHAR NOT NULL,
-                            username CHAR NOT NULL,
-                            password CHAR NOT NULL,
-                            address CHAR NOT NULL,
-                            city CHAR NOT NULL,
-                            state CHAR NOT NULL,
-                            country CHAR NOT NULL,
-                            zip INT NOT NULL);
-                        """)
--- app/utils/test_utils/date_seeder.py
+++ app/utils/test_utils/date_seeder.py
@@ -2,43 +2,56 @@
 
 import csv
 from users.models import Host
-with open('host_directory.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            try:
-                print("There are rows: " + str(len(row)))
-                username = row[0]
-                password = row[1]
-                first_name = row[2]
-                last_name = row[3]
-                email = row[4]
-                phone_number = row[5]
-                state = row[6]
-                # city = row[8]
-                # address = row[9]
-                # zip = row[10]
-                # host.username = username
-                # host.password = password
-                # host.address = address
-                # host.city = city
-                # host.state  =state
-                # host.zip = zip
-                # host.phone_number = phone_number
-                host = Host()
-                host.first_name = first_name
-                host.last_name = last_name
-                host.email = email
-                host.save()
-                print("Processed a host")
+
+def seed_host(host_csv):
+    with open(host_csv) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                print(f'Column names are {", ".join(row)}')
                 line_count += 1
-            except Exception as e:
-                print(e)
+            else:
+                try:
 
-    print(f'Processed {line_count} lines.')
+                    host = Host()
 
+                    username = row[0]
+                    host.username=username
 
+                    password = row[1]
+                    host.password=password
+
+                    first_name = row[2]
+                    host.first_name = first_name
+
+                    last_name = row[3]
+                    host.last_name = last_name
+
+                    email = row[5]
+                    host.email = email
+
+                    phone_number = row[6]
+                    host.phone_number=phone_number
+
+                    state = row[7]
+                    host.state=state
+
+                    city = row[8]
+                    host.city = city
+
+                    address = row[9]
+                    host.address=address
+
+                    zip = int(row[10])
+                    host.zip = zip
+
+                    host.save()
+                    print("Processed a host")
+                    line_count += 1
+                except Exception as e:
+                    print(e)
+
+        print(f'Processed {line_count} lines.')
+
+seed_host('hosts.csv')
