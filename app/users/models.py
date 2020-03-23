d55929cb374b8275acee91897fbac6a9cef7737c
Mathias Darr <mddarr@gmail.com>
2020-03-22 21:42:07 -0700

    Swithc the guest model up a bit, add notes on how to delete the database properly

--- app/users/models.py
+++ app/users/models.py
@@ -1,18 +1,17 @@
 from django.db import models
-
 class Guest(models.Model):
+    username = models.CharField(max_length=30, default="user1")
+    password = models.CharField(max_length=30, default = "iksarman")
     first_name = models.CharField(max_length=30, default="Joseph")
     last_name = models.CharField(max_length=30, default="Dulapp")
-    email = models.CharField(max_length=30, default="")
-    phone_number = models.CharField(max_length=15, default=" ")
-    username = models.CharField(max_length=30)
-    password = models.CharField(max_length=30)
+    email = models.CharField(max_length=30, default="Nebrasa")
+    phone_number = models.CharField(max_length=35, default=" ")
     address = models.CharField(max_length=60, default='8009 39th AVE NE')
     city = models.CharField(max_length=30, default='Seattle')
     state = models.CharField(max_length=30, default='Washington')
     country = models.CharField(max_length=30, default='United States')
     zip = models.IntegerField( default=98115)
-
+    
 class Host(models.Model):
     username = models.CharField(max_length=30, default="firstTab.txt")
     password = models.CharField(max_length=30, default = "pass123")
