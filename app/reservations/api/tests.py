210ca866429bb12c163b1a1d93c98477529916c3
Mathias Darr <mddarr@gmail.com>
2020-03-23 14:41:13 -0700

    Add a test that verifies that the reservations API will return a 409 (conflict) when the submitted dates intersect with the resevered dates.  Excellent!

--- app/reservations/api/tests.py
+++ app/reservations/api/tests.py
@@ -15,13 +15,6 @@ class RoomTestCase(APITestCase):
 
 
     def test_book_reservation(self):
-        hosts = list(Host.objects.all())
-        print("There are " + str(len(hosts)) + " hosts in the DB")
-        guests = list(Guest.objects.all())
-        print("There are " + str(len(guests)) + " guests in the DB")
-        rooms = list(Room.objects.all())
-        print("There are " + str(len(rooms)) + " rooms in the DB")
-
         # The first guest in the database will make a booking
         guestID = Guest.objects.all().aggregate(Min('id'))['id__min'] #
         roomID = Room.objects.all().aggregate(Min('id'))['id__min']  #
@@ -41,3 +34,39 @@ class RoomTestCase(APITestCase):
         # Assert that the dates were appeneded to the list
         room = Room.objects.get(id=roomID)
         self.assertEqual(len(room.reserved_dates), 4)
+
+
+    def test_book_reservation_reserved_dates(self):
+        # The first guest in the database will make a booking
+        guest1_ID = Guest.objects.all().aggregate(Min('id'))['id__min'] #
+        roomID = Room.objects.all().aggregate(Min('id'))['id__min']  #
+
+        booking_create_response = self.client.post(
+            path="http://127.0.0.1:8000/reservations/book/",
+            data=json.dumps({
+                "room":roomID,
+                "guest":guest1_ID,
+                'checkin_date':'2020-04-15',
+                'checkout_date': '2020-04-18'
+            }),
+            content_type='application/json'
+        )
+        self.assertEqual(booking_create_response.status_code, status.HTTP_201_CREATED)
+
+        # Assert that the dates were appeneded to the list
+        room = Room.objects.get(id=roomID)
+        self.assertEqual(len(room.reserved_dates), 4)
+
+        guest2_ID = Guest.objects.all().aggregate(Min('id'))['id__min'] + 1
+
+        booking_create_response = self.client.post(
+            path="http://127.0.0.1:8000/reservations/book/",
+            data=json.dumps({
+                "room":roomID,
+                "guest":guest2_ID,
+                'checkin_date':'2020-04-17',
+                'checkout_date': '2020-04-19'
+            }),
+            content_type='application/json'
+        )
+        self.assertEqual(booking_create_response.status_code, status.HTTP_409_CONFLICT)
--- app/reservations/api/views.py
+++ app/reservations/api/views.py
@@ -17,8 +17,8 @@ from rooms.models import Room
 This could and should be moved to a method on the Room model
 '''
 
-def add_dates(roomID, dates):
-    room = Room.objects.get(id=roomID)
+def add_dates(room, dates):
+
     [room.reserved_dates.append(str(day)) for day in dates]
     room.save()
 
@@ -28,20 +28,36 @@ def remove_dates(roomID, checkInDate, checkOutDate):
     [room.reserved_dates.remove(str(day)) for day in dates]
     room.save()
 
+def validate_dates(room, dates):
+    reserved_dates = room.reserved_dates
+    print("reserveed dates " + str(reserved_dates))
+    print("Incoming dates " + str(dates))
+    for date in dates:
+        if str(date) in reserved_dates:
+            return False
+    # print("Validation is not working!")
+    return True
+
 class ReservationDetailView(APIView):
 
     def post(self,request,*args,**kwargs):
         reservation_serializer = ReserverationSerializer(data=request.data)
         if reservation_serializer.is_valid():
             roomID = request.data['room']
+            room = Room.objects.get(id=roomID)
+
             startDate = reservation_serializer.validated_data['checkin_date']
             endDate = reservation_serializer.validated_data['checkout_date']
 
             dates = date_range_list(startDate, endDate)
+            print("The type of room is " + str(type(room)))
+
             if len(dates) == 0:
                 return Response("Invalid date selection", status=status.HTTP_400_BAD_REQUEST)
+            if not validate_dates(room, dates):
+                return Response("Invalid date selection", status=status.HTTP_409_CONFLICT)
 
-            add_dates(roomID, dates)
+            add_dates(room, dates)
             reservation_serializer.save()
             return Response(reservation_serializer.data, status=status.HTTP_201_CREATED)
         else:
